c9ee193e9633057052d220629f86f96a7f43b3a3
Jholic <tmdgh8909@gmail.com>
2025-10-12 11:33:54 +0900

    ??
    ??

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'action.manager.app',
+  appName: 'Action Manager',
+  webDir: 'dist'
+};
+
+export default config;
